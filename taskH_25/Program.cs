1fd5d1c4378539eab0b90eb499987e9d45f9a674
Pavel <rut100500@yandex.ru>
2022-11-12 19:50:13 +0300

    Зачача 25 - возвести число A в натуральную степень B

---
+++ taskH_25/Program.cs
@@ -0,0 +1,34 @@
+﻿// Цикл, принимает на вход жва числа (A и B) и возводит число A в нутуральную стеень B
+Console.WriteLine("Введите основание степени");
+bool isParsedA = int.TryParse(Console.ReadLine(), out int a);
+
+Console.WriteLine("Введите показатель степени");
+bool isParsedB = int.TryParse(Console.ReadLine(), out int b);
+
+if(!isParsedA || !isParsedB)
+{
+    Console.WriteLine("Ошибка. Не то ввели! ПереВВедите!");
+    return;
+}
+
+
+if (b < 0)
+{
+    Console.WriteLine("Не та цифра");
+    return;
+}
+
+int result = GetResNum(b);
+Console.WriteLine($"{a} в степени {b} равно {result}");
+
+
+int GetResNum(int number)
+{
+    int res = 1;
+    for( int i = 1; i <= number; i++)
+    {
+        res = a * res;
+        //Console.WriteLine(res);
+    }
+    return res;
+}
